69b8f1aa2c1dcd379c16834c15274e28a6479e7c
Adeleye Remi-Alarape <57394576+adeleye-art@users.noreply.github.com>
2024-03-06 16:59:23 +0100

    update

--- findadeleye/src/Portfolio.jsx
+++ findadeleye/src/Portfolio.jsx
@@ -2,7 +2,8 @@
 
     import rnleo from './assets/rnleo.jpg'
     import toDo from './assets/toDo.png'
-    
+    import calc from './assets/calc.png'
+    import quiz from './assets/quiz.png'
 
 function Portfolio(){
     const projects = [
@@ -28,6 +29,29 @@ function Portfolio(){
             details: "As a part of my personal projects, I developed a Todo App that helps users manage their tasks efficiently. The application provides a user-friendly interface to add, edit, and delete tasks, with a real-time update feature for seamless user experience.",
             moreDetails:"Utilizing React.js for the frontend and incorporating state management for efficient data handling, the Todo App showcases my proficiency in building interactive and responsive web applications. The project also demonstrates my commitment to creating solutions that enhance productivity and organization.",
             
+        },
+        {
+            headingImg: <img src={calc} alt="calc app" />,
+            client: 'Personal project',
+            type: 'web app',
+            date: '2024',
+            address: 'online',
+            title: 'Calculator',
+            links:<a href="https://investcalculate.netlify.app/" target="_blank" rel="noreferrer">Calculator</a>,
+            details:"This project is a basic investment calculator that allows users to perform basic investment operations.",
+            moreDetails:"In conclusion, we successfully built an investment calculator application using React, incorporating a currency converter function to provide users with flexibility in viewing investment results in different currencies. By leveraging React's component-based architecture and state management capabilities, we created a robust and user-friendly application suitable for financial planning and analysis.  "
+
+        },
+        {
+            headingImg: <img src={quiz} alt="Quiz app" />,
+            client: 'Personal project',
+            type: 'web app',
+            date: '2024',
+            address: 'online',
+            title: 'Quiz App',
+            links:<a href="https://quiz-art.netlify.app/" target="_blank" rel="noreferrer">Quiz App</a>,
+            details:"This project is a basic quiz app that allows users to test their knowledge of different topics.",
+            moreDetails:"The Quiz App is a personal project, a web application accessible online, designed to test users' knowledge across various topics. Featuring a user-friendly interface, it offers multiple-choice questions, scoring, and feedback, all within a responsive design. Developed using HTML, CSS, and JavaScript, it serves as both a prototype and an engaging platform for knowledge expansion."
         }
     ];
 
